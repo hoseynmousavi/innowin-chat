d8f4c9295da99741c84dd415dad6bd66050895b5
Abolfazl <miri1888@gmail.com>
2019-08-25 16:56:25 +0430

    bug fix

--- src/index.js
+++ src/index.js
@@ -20,46 +20,44 @@ const wss = new WebSocket.Server({server: app.listen(5000)})
 
 let arr = {}
 
-wss.on("connection", (ws, req) =>
-{
-    const userId = req.url.split("/?id=")[1]
-    arr[userId] = {ws, token: null}
-    ws.on("close", () => arr[userId].ws = null)
+wss.on("connection", (ws, req) => {
+  const userId = req.url.split("/?id=")[1]
+  const prevUser = arr[userId] ? {...arr[userId]} : {}
+  arr[userId] = {...prevUser, ws}
+  ws.on("close", () => arr[userId].ws = null)
 })
 
 app.route("/sendMessage")
-    .post((req, res) =>
-    {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        const data = {...req.body}
-        const {receiver} = data
-        if (receiver && arr[receiver] && arr[receiver].ws)
-        {
-            arr[receiver].ws.send(JSON.stringify(data))
-            res.send({state: 1, message: "message sent to the user"})
-        }
-        else if (arr[receiver] && arr[receiver].token)
-        {
-            arr[receiver].token && webpush.sendNotification(arr[receiver].token, JSON.stringify({title: "پیام رسان اینوین", body: data.text, icon: 'https://innowin.ir/icon-192x192.png'})).catch(err => console.error(err))
-            res.send({state: -2, message: "user is not online"})
-        }
-        else
-        {
-            res.send({state: -1, message: "user is not online & wasn't"})
-        }
+    .post((req, res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      const data = {...req.body}
+      const {receiver} = data
+      if (receiver && arr[receiver] && arr[receiver].ws) {
+        arr[receiver].ws.send(JSON.stringify(data))
+        res.send({state: 1, message: "message sent to the user"})
+      }
+      else if (arr[receiver] && arr[receiver].token) {
+        arr[receiver].token && webpush.sendNotification(arr[receiver].token, JSON.stringify({
+          title: "پیام رسان اینوین",
+          body: data.text,
+          icon: "https://innowin.ir/icon-192x192.png",
+        })).catch(err => console.error(err))
+        res.send({state: -2, message: "user is not online"})
+      }
+      else {
+        res.send({state: -1, message: "user is not online & wasn't"})
+      }
     })
 
 app.route("/")
-    .get((req, res) =>
-    {
-        res.send("Hello Babes!")
+    .get((req, res) => {
+      res.send("Hello Babes!")
     })
 
 app.route("/subscribe")
-    .post((req, res) =>
-    {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        const {subscription, userId} = req.body
-        if (arr[userId]) arr[userId].token = subscription
-        res.sendStatus(200)
+    .post((req, res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      const {subscription, userId} = req.body
+      if (arr[userId]) arr[userId].token = subscription
+      res.sendStatus(200)
     })
